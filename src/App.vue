c92f839921c9736f50b5e803804be17336747e6e
Arun Cheerla <aruncheerla@Aruns-MacBook-Air.local>
2022-07-12 15:19:02 -0500

    login-update-A

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-      <v-app-bar  >
+      <v-app-bar >
             <v-img
                 class="mx-2"
                 :src="logo"
@@ -11,36 +11,36 @@
             <v-app-bar-title  >My Surveys</v-app-bar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items>
-              <v-btn 
+              <v-btn v-if="$route.name == 'home' || $route.name == 'login' || $route.name == 'register' || $route.name == 'about'"
                     variant="text"
                     @click="goHome" >
                   Home
                 </v-btn>
-                <v-btn 
+                <v-btn v-if="$route.name == 'home' || $route.name == 'login' || $route.name == 'register' || $route.name == 'about'"
                     variant="text"
                     @click="goAbout"
                     >
                   About Us
                 </v-btn>
-                <v-btn 
+                <v-btn v-if="$route.name == 'add' || $route.name == 'tutorials'"
                     variant="text"
                     @click="goList"
                     >
                   List
                 </v-btn>
-                <v-btn 
+                <v-btn v-if="$route.name == 'add' || $route.name == 'tutorials'"
                     variant="text"
                     @click="goAdd"
                     >
                   Add Survey
                 </v-btn>
-                <v-btn 
+                <v-btn v-if="$route.name == 'home' || $route.name == 'login' || $route.name == 'register' || $route.name == 'about'"
                     variant="text"
                     @click="goLogin"
                     >
                   Login
                 </v-btn>
-                <v-btn 
+                <v-btn v-if="$route.name == 'home' || $route.name == 'login' || $route.name == 'register' || $route.name == 'about'"
                     variant="text"
                     @click="goRegister"
                     >
@@ -55,17 +55,17 @@
         </div>
           <router-view @authenticated="setAuthenticated" />
         </v-container>
-      </v-main>  
+      </v-main>
 
       <div class="footer">
            <div class="container">
-             <p>© Copyright 2022 My Surveys. All rights reserved.<v-btn 
+             <p>© Copyright 2022 My Surveys. All rights reserved.<v-btn
                     variant="text"
                     @click="goPrivacy"
                     >
                   Privacy Policy
                 </v-btn></p>
-           </div>   
+           </div>
       </div>
 
     </v-app>
@@ -84,38 +84,48 @@ export default {
                 mockAccount: {
                     username: "bharath",
                     password: "test"
-                }
+                },
+                url:""
             }
-
   }),
 
   mounted() {
             if(!this.authenticated) {
                 this.$router.replace({ name: "login" });
+                this.url="login"
             }
+
         },
 
   methods: {
     goHome() {
       this.$router.push({ name: 'home' });
+      this.url="home"
     },
     goAdd() {
       this.$router.push({ name: 'add' });
+      this.url="add"
     },
     goAbout() {
       this.$router.push({ name: 'about' });
+      this.url="about"
     },
     goPrivacy() {
       this.$router.push({ name: 'privacy' });
+      this.url="privacy"
     },
     goList() {
       this.$router.push({ name: 'tutorials' });
+      this.url="tutorials"
+      this.addFlag=true
     },
     goLogin() {
       this.$router.push({ name: 'login' });
+      this.url="login"
     },
     goRegister() {
       this.$router.push({ name: 'register' });
+      this.url="register"
     },
     setAuthenticated(status) {
                 this.authenticated = status;
